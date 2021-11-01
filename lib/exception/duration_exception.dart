196d404af66ddc0e3d740808d0856e382be5de30
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-11-01 21:24:26 +0100

    k

--- lib/exception/duration_exception.dart
+++ lib/exception/duration_exception.dart
@@ -2,6 +2,8 @@ import 'package:flutter_camp/core/enum/duration_enum.dart';
 
 class DurationException implements Exception {
   late final DurationEnums data;
+
+  DurationException(DurationEnums durationEnums);
   @override
   String toString() {
     return "Duration not found $data";
